b20b1b8f13d1e0b37cf85e2d698475930fd4eb5f
jjdub82 <jjwood82@gmail.com>
2023-05-17 19:59:21 -0500

    individual pages

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const resultsPerPage = 20; // This can be any number up to the API's limit
     const from = currentPage * resultsPerPage;
     const to = from + resultsPerPage;
-
+    // const detail_button = document.getElementsByClassName('details-button')
     const app_id = 'a3d1dcf8'
     const app_key = 'b638b6abb916f1a4882edc001a786986'
+
+    // https://api.edamam.com/api/recipes/v2?type=any&q=Chicken&app_id=a3d1dcf8&app_key=b638b6abb916f1a4882edc001a786986
 
     const fetchRecipes = () => {
         const searchText = searchInput.value;
@@ -19,7 +21,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const to = from + resultsPerPage;
 
     if (searchText.length > 2) {
-        const url = `https://api.edamam.com/search?q=${searchText}&app_id=${app_id}&app_key=${app_key}&from=${from}&to=${to}`;
+        const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchText}&app_id=${app_id}&app_key=${app_key}&from=${from}&to=${to}`;
 
         fetch(url)
             .then(response => response.json())
@@ -30,59 +32,117 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     const recipe = hit.recipe;
 
                     const item = document.createElement('div');
-                    item.className = 'item p-5 mx-3';
-                    item.style.outline = '1px solid lightgray; border-radius: 10px';
+                        item.className = ' item p-5 mx-3';
+                        item.style.outline = '1px solid lightgray; border-radius: 10px';
 
-                    const img = document.createElement('img');
-                    img.src = recipe.image;
-                    img.alt = 'recipe image';
-                    item.appendChild(img);
+                        const flex = document.createElement('div');
+                        flex.className = 'flex-wrapper';
 
-                    const h1 = document.createElement('h1');
-                    h1.className = 'title';
-                    h1.textContent = recipe.label;
-                    item.appendChild(h1);
 
-                    const a = document.createElement('a');
-                    a.className = 'view-button';
-                    a.href = recipe.url;
-                    a.textContent = 'View Recipe';
-                    item.appendChild(a);
+                        // Create the row
+                        const row = document.createElement('div');
+                        row.className = 'row';
 
-                    const p = document.createElement('p');
-                    p.className = 'item-data';
-                    p.textContent = `Calories: ${Math.round(recipe.calories/recipe.yield)}`;
-                    item.appendChild(p);
 
-                    const dishtype = document.createElement('p');
-                    dishtype.className = 'item-data';
-                    dishtype.textContent = recipe.dishType.join(', ');
-                    item.appendChild(dishtype);
+                        // Create the first column
+                        const column1 = document.createElement('div');
+                        column1.className = 'col-md-6';
 
-                    const cuisineType = document.createElement('p');
-                    cuisineType.className = 'item-data';
-                    cuisineType.textContent = 'Cuisine' +' '+recipe.cuisineType.join(', ');
-                    item.appendChild(cuisineType);
+                        const img = document.createElement('img');
+                        img.src = recipe.image;
+                        img.alt = 'recipe image';
+                        img.style.width = '200px'; // Set the desired width
+                        img.style.height = '150px'; // Set the desired height
+                        column1.appendChild(img);
 
-                    const details = document.createElement('a');
-                    details.className = 'view-button';
-                    details.href = `detail.html?recipe=${encodeURIComponent(JSON.stringify(recipe))}`;
-                    details.textContent = 'View Details';
-                    item.appendChild(details);
-        
+                        const h1 = document.createElement('h1');
+                        h1.className = 'title';
+                        h1.textContent = recipe.label;
+                        column1.appendChild(h1);
 
-                    const yield = document.createElement('p');
-                    yield.className = 'item-data';
-                    yield.textContent = 'Serves' +' '+ recipe.yield;
-                    item.appendChild(yield)
+                        const a = document.createElement('a');
+                        a.className = 'view-button';
+                        a.href = recipe.url;
+                        a.textContent = 'View Recipe';
+                        column1.appendChild(a);
 
-                    
+                        const p = document.createElement('p');
+                        p.className = 'item-data';
+                        p.textContent = `Calories: ${Math.round(recipe.calories / recipe.yield)}`;
+                        column1.appendChild(p);
 
-                    
+                        const dishtype = document.createElement('p');
+                        dishtype.className = 'item-data';
+                        dishtype.textContent = recipe.dishType.join(', ');
+                        column1.appendChild(dishtype);
 
-                    searchResult.appendChild(item);
+                        const cuisineType = document.createElement('p');
+                        cuisineType.className = 'item-data';
+                        cuisineType.textContent = 'Cuisine' + ' ' + recipe.cuisineType.join(', ');
+                        column1.appendChild(cuisineType);
+
+                        const yield = document.createElement('p');
+                        yield.className = 'item-data yield';
+                        yield.textContent = 'Serves' + ' ' + recipe.yield;
+                        column1.appendChild(yield);
+
+                        // Append the first column to the item div
+                        item.appendChild(column1);
+
+                        // Create the second column
+                        const column2 = document.createElement('div');
+                        column2.className = 'col-md-6';
+
+                        const detailsButton = document.createElement('button');
+                        detailsButton.className = 'details-button';
+                        detailsButton.textContent = 'View Details';
+                        column2.appendChild(detailsButton);
+
+                        const dietLabels = document.createElement('p');
+                        dietLabels.className = 'item-data';
+                        dietLabels.textContent = recipe.dietLabels;
+                        column2.appendChild(dietLabels);
+
+                        const recipeData = JSON.stringify(recipe);
+                        const encodedRecipeData = encodeURIComponent(recipeData);
+                        const url = `detail.html?recipeId=${encodeURIComponent(recipe.uri.split('_')[1])}`;
+                        const recipeUri = document.createElement('a');
+                        recipeUri.className = 'btn btn-primary';
+                        recipeUri.textContent = 'Details';
+                        recipeUri.href = url;
+
+                        column2.appendChild(recipeUri);
+
+
+                        // https://api.edamam.com/api/recipes/v2/f4ce5814f10c79bf856f7d40b2bade83?type=public&app_id=a3d1dcf8&app_key=b638b6abb916f1a4882edc001a786986
+
+
+                        // Append the second column to the item div
+                        item.appendChild(column2);
+
+                        // Append the item div to the searchResult
+                        searchResult.appendChild(item);
+
+                        // Add event listener to each details button
+                        detailsButton.addEventListener('click', () => {
+                        const itemContainer = detailsButton.closest('.item');
+                        const recipeLabel = itemContainer.querySelector('.title').textContent;
+                        const recipeImage = itemContainer.querySelector('img').src;
+                        const recipeURL = itemContainer.querySelector('a.view-button').href;
+                        const recipeYield = itemContainer.querySelector('.yield').textContent;
+
+                        console.log('Recipe Label:', recipeLabel);
+                        console.log('Recipe Image:', recipeImage);
+                        console.log('Recipe URL:', recipeURL);
+                        console.log('Recipe Yield:', recipeYield);
+
+                        // Open the modal and populate its content with the recipe details
+                        // (Code to open and populate the modal goes here)
+                        });
+
                 });
             })
+                    
             .catch(error => console.error('Error:', error));
     }
     }
@@ -104,12 +164,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         fetchRecipes();
     });
 });
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const recipeString = urlParams.get('recipe');
-    const recipe = JSON.parse(decodeURIComponent(recipeString));
-  
-    // Access and use the recipe object's attributes as needed...
+const detailsButtons = document.getElementsByClassName('details-button');
+Array.from(detailsButtons).forEach(detailsButton => {
+  detailsButton.addEventListener('click', () => {
+    // Create variables for each attribute of the clicked recipe
+    const { uri, label, image, source, url, yield: recipeYield, dietLabels, healthLabels, ingredientLines } = selectedRecipe;
+    console.log(selectedRecipe);
+    // Rest of your code...
   });
-  
+});
+                
---
+++ recipe.js
@@ -0,0 +1,127 @@
+const urlParams = new URLSearchParams(window.location.search);
+const recipeId = urlParams.get('recipeId');
+const app_id = 'a3d1dcf8'
+const app_key = 'b638b6abb916f1a4882edc001a786986'
+const recipeContainer = document.getElementById('recipeContainer');
+
+recipe: {
+  ingredientLines: []
+}
+;
+
+
+
+console.log(recipeId)
+  
+const apiUrl = `https://api.edamam.com/api/recipes/v2/${recipeId}?type=public&app_id=${app_id}&app_key=${app_key}`;
+console.log(apiUrl)
+  
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+
+    // Process the recipe data here
+  
+    // console.log(data);
+    const recipe = data.recipe
+    console.log(recipe)
+
+    //create the div
+    const item = document.createElement('div');
+    item.className = ' item p-5 mx-3 container-fluid';
+    item.style.outline = '1px solid lightgray; border-radius: 10px';
+    recipeContainer.appendChild(item);
+
+    //  // Create the row
+    //  const row = document.createElement('div');
+    //  row.className = 'row';
+
+    const row1 =document.createElement('div')
+    row1.className = 'row';
+    
+    const row2 = document.createElement('div')
+    row2.className = 'row mt-3'
+    
+
+    const col1 =document.createElement('div')
+    col1.className = 'col-md-4';
+    col1.style.outline = '1px solid lightgray';
+    
+
+    const col2 =document.createElement('div')
+    col2.className = 'col-md-6';
+    col2.style.outline = '1px solid lightgray';
+   
+  
+     const img = document.createElement('img');
+        img.src = recipe.image;
+        img.alt = 'recipe image';
+        img.style.width = '200px'; // Set the desired width
+        img.style.height = '150px'; // Set the desired height
+        col1.appendChild(img);
+
+      const recipeLabel = document.createElement('h1');
+      recipeLabel.className = 'display-1';
+      recipeLabel.textContent = recipe.label
+      col1.appendChild(recipeLabel);
+
+ 
+
+      const yield = document.createElement('p');
+      yield.className = 'item-data';
+      yield.textContent = 'Yields '+ recipe.yield + ' servings'
+      col2.appendChild(yield);
+
+      const calories = document.createElement('p');
+      calories.className = 'item-data';
+      calories.textContent = `Calories: ${Math.round(recipe.calories / recipe.yield)}` + ' per serving'
+      col2.appendChild(calories);
+
+      const mealType = document.createElement('p');
+      mealType.className = 'item-data';
+      mealType.textContent = recipe.mealType;
+      col2.appendChild(mealType);
+
+      const dietLabels = document.createElement('p');
+      dietLabels.className = 'item-data';
+      dietLabels.textContent = recipe.dietLabels;
+      col2.appendChild(dietLabels);
+
+      const health = document.createElement('p');
+      health.className = 'item-data';
+      health.textContent = recipe.healthLabels;
+      col2.appendChild(health)
+
+      const indgredients = document.createElement('p');
+      indgredients.className = 'item-data mt-3';
+      indgredients.textContent = recipe.ingredientLines;
+      row2.appendChild(indgredients);
+      
+      const ingredientLines = recipe.ingredientLines;
+      if (ingredientLines.length > 0) {
+        ingredientLines.forEach(item => {
+        const lineElement = document.createElement('p');
+        lineElement.className ='item-data mt-2 pt-2'
+        lineElement.textContent = item;
+        row2.appendChild(lineElement);
+        });
+      } else {
+        const emptyElement = document.createElement('p');
+        emptyElement.textContent = 'No ingredients found.';
+        row2.appendChild(emptyElement);
+      }
+
+      
+
+
+    row1.appendChild(col1);
+    row1.appendChild(col2);
+
+    item.appendChild(row1);
+    item.appendChild(row2);
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
